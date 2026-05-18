968d2980fccc0862967f2941d2af2d7f7e2bfc8e
luizfelipelustosa@gmail.com <122307110+Luizfsl@users.noreply.github.com>
2026-05-18 03:54:47 +0000

    chore: correção de compatibilidade

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { Request, Response } from 'express';
 import userRoutes from './routes'; 
 import { router as movieRoutes } from './routes/movie-routes';
@@ -5,11 +6,15 @@ import { router as movieRoutes } from './routes/movie-routes';
 const app = express();
 app.use(express.json());
 
+// Registrando as rotas
 app.use(userRoutes);
+
+// Todas as rotas de filmes
 app.use("/", movieRoutes);
 
+// Rota principal de verificação da API
 app.get('/', (req: Request, res: Response) => {
-  res.json({ mensagem: "API funcionando!" });
+  res.json({ message: "Movie Streaming API is running" });
 });
 
 app.listen(3000, () => {
